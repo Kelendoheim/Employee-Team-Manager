c395a6871fe7779f51af884fd478d9f1590d91d2
TDWalsh <thomas.walsh736@gmail.com>
2020-09-30 00:53:25 -0400

    added comments into the code

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 const employeeArray = [];
-
+// the following function will have all other functions inside and will do everything
 function promptUser() {
   inquirer
     .prompt([
@@ -22,6 +22,7 @@ function promptUser() {
       },
     ])
     .then(function (answers) {
+        //the following conditional will run the corresponding function to generate a new object from the associated class
       if (answers.addEmployee === "Yes") {
         employeeType()
           .then(function (answers) {
@@ -55,6 +56,7 @@ function promptUser() {
             console.log(err);
           });
       } else {
+          //if the user declines to add another team member the html will be generated
         const employeeHTML = render(employeeArray);
         fs.writeFile(outputPath, employeeHTML, function(err) {
 
@@ -71,7 +73,7 @@ function promptUser() {
       console.log(err);
     });
 }
-
+// this function will check to see which type of employee the user wants to add
 function employeeType() {
   return inquirer.prompt([
     {
@@ -82,7 +84,7 @@ function employeeType() {
     },
   ]);
 }
-
+//this function will create an object from the Intern class and push it into the employee array
 function internData() {
   return inquirer
     .prompt([
@@ -108,7 +110,6 @@ function internData() {
       },
     ])
     .then(function (answers) {
-      // console.log(answers.name, answers.id, answers.email, answers.school)
       const internObj = new Intern(
         answers.name,
         answers.id,
@@ -121,6 +122,7 @@ function internData() {
       console.log(err);
     });
 }
+//this function will create an object from the Engineer class and push it into the employee array
 function engineerData() {
   return inquirer
     .prompt([
@@ -146,7 +148,6 @@ function engineerData() {
       },
     ])
     .then(function (answers) {
-      // console.log(answers.name, answers.id, answers.email, answers.github)
       const engineerObj = new Engineer(
         answers.name,
         answers.id,
@@ -159,6 +160,7 @@ function engineerData() {
       console.log(err);
     });
 }
+//this function will create an object from the Manager class and push it into the employee array
 function managerData() {
   return inquirer
     .prompt([
@@ -184,7 +186,6 @@ function managerData() {
       },
     ])
     .then(function (answers) {
-      // console.log(answers.name, answers.id, answers.email, answers.office)
       const managerObj = new Manager(
         answers.name,
         answers.id,
@@ -199,25 +200,4 @@ function managerData() {
 }
 promptUser();
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
